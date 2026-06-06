9514fc33a9372e58e572aac59fd13b30baeb9749
abhinavsingh11-afk <ashadevi1982nn@gmail.com>
2026-06-06 21:04:31 +0530

    Add files via upload

---
+++ license project.py
@@ -0,0 +1,10 @@
+print("What is your age?")
+age = int(input())
+
+if age > 18 :
+	print("u can drive car")
+elif age == 18 :
+	print("we will think about you")
+else :
+	print("u cannot drive car")
+	
